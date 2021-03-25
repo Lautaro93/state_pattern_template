9d75151473c648e66cd756d8fc413846a36bbf15
Lautaro93 <46577166+Lautaro93@users.noreply.github.com>
2021-03-25 13:07:22 +0100

    Added comments in main.c

--- main.c
+++ main.c
@@ -16,28 +16,30 @@ int main(void)
     /* Enable global interrupts. */
     CyGlobalIntEnable; 
 
+    /* Initializes PSoC peripherals and interrupts */
     UART_RF_Start();
     UART_RF_EnableRxInt();
-    INT_RX_ClearPending();                                                   //Borra les interrupcions pendents
+    INT_RX_ClearPending();
     INT_RX_StartEx(isr_rx); 
     
     Serial serial;
     serial_construct(&serial);
     
-//    current_input = Forward;
+    /* Change this input to test the state machine */
+    current_input = Forward
     
     for(;;)
     {
-        /* Place your application code here. */
+        /* Query for a new char from serial */
         if(serial.is_buffer_ready(&serial))
         {
+            /* Prints an echo over serial from the word received */
             if(serial.read_buffer(&serial, '\n'))
                 printf("%s\n", serial.rx_str);
         }
                 
-//        fsm_update();
+        /* Update the state machine */
+        fsm_update();
     }
 }
-
-
 /* [] END OF FILE */
