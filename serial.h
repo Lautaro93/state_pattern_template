ddf4c6b1295ed3662a7ce9c411c7c26a7134ceee
Lautaro93 <46577166+Lautaro93@users.noreply.github.com>
2021-03-25 13:13:31 +0100

    Added comments to serial.h

--- serial.h
+++ serial.h
@@ -14,12 +14,16 @@
     
 #include "header.h"
 
+/* Declaration of circular buffer of 32 chars length */
 uint8	rx_buffer[32];
+/* Declaration of read and write pointers */
 extern uint8 *rx_read_ptr;
 extern uint8 *rx_write_ptr;
 
+/* PSoC prototype of the serial interrupt */
 CY_ISR_PROTO(isr_rx);
 
+/* Definition of the struct serial, modelated as an object */
 typedef struct _serial
 {
     // Atributtes
@@ -35,8 +39,8 @@ typedef struct _serial
     uint8 (*read_buffer)(struct _serial *this, char eom);
 }Serial;
 
+/* Prototype of the public construct */
 void serial_construct(struct _serial *this);
-
 
 #endif
 /* [] END OF FILE */
